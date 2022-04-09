92580069d8220dd6d04bc83c5a7f9d291f783aa6
Thomas D <72820355+Kierha@users.noreply.github.com>
2022-04-09 15:01:01 +0200

    rename auth folder

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:trinity/Screens/Authentification/AuthentificationScreen.dart';
+import 'package:trinity/Screens/AuthentificationPage/AuthentificationScreen.dart';
 import 'package:trinity/screens/LogoPage/LogoPage.dart';
 
 void main() async {
