1c8f2d67cf49ffa580888b1403ae1dc2604a98df
Thomas D <72820355+Kierha@users.noreply.github.com>
2022-04-13 16:18:11 +0200

    save

--- lib/Screens/classChoiceScreen.dart/classChoiceScreen.dart
+++ lib/Screens/classChoiceScreen.dart/classChoiceScreen.dart
@@ -10,85 +10,95 @@ class ClassChoiceScreen extends StatefulWidget {
 
 class _ClassChoiceScreenState extends State<ClassChoiceScreen> {
   @override
+  String text = 'blablablabla';
+
+  void changeData() {
+    setState(() {
+      text = 'lalalala';
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.blueAccent,
-          elevation: 0.0,
-          title: const Text(
-            'Choose your Role',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'Lemon Tea',
-            ),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+        elevation: 0.0,
+        title: const Text(
+          'Choose your Role',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Lemon Tea',
           ),
         ),
-        backgroundColor: const Color.fromRGBO(24, 24, 25, 1.0),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  color: Colors.orange,
-                  margin: const EdgeInsets.all(25.0),
-                  child: const FlutterLogo(
-                    size: 60.0,
-                  ),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  color: Colors.blue,
-                  margin: const EdgeInsets.all(5.0),
-                  child: Image.asset(
-                    'assets/images/TankIcone.png',
-                    width: 100.0,
-                    height: 100.0,
-                  ),
+      ),
+      // backgroundColor: const Color.fromRGBO(24, 24, 25, 1.0),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.orange,
+                margin: const EdgeInsets.all(25.0),
+                child: const FlutterLogo(
+                  size: 60.0,
                 ),
-                Container(
-                  color: Colors.blue,
-                  margin: const EdgeInsets.all(5.0),
-                  child: Image.asset(
-                    'assets/images/DpsIcone.png',
-                    width: 100.0,
-                    height: 100.0,
-                  ),
-                ),
-                Container(
-                  color: Colors.blue,
-                  margin: const EdgeInsets.all(5.0),
-                  child: Image.asset(
-                    'assets/images/HealerIcone.png',
-                    width: 100.0,
-                    height: 100.0,
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                RoleContainer(
-                  child: Flexible(
-                    child: Text(
-                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam porta dui vitae diam volutpat, at volutpat leo sodales. Sed pulvinar libero eget elit interdum gravida. Suspendisse potenti. Quisque lacinia lacinia elit, non aliquet ipsum ullamcorper sed. Duis libero urna, egestas scelerisque diam ut, interdum vulputate erat. Maecenas sed dui nec justo aliquam scelerisque. Donec et tellus vitae mi mattis facilisis. Sed imperdiet leo non aliquet egestas. In orci risus, tincidunt quis porta sit amet, euismod at enim.',
-                      softWrap: true,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/images/TankIcone.png',
+                      width: 100.0,
+                      height: 100.0,
                     ),
-                  ),
-                )
-              ],
-            )
-          ],
-        ));
+                    Text('Tank'),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/images/HealerIcone.png',
+                      width: 100.0,
+                      height: 100.0,
+                    ),
+                    Text('Healer')
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/images/DpsIcone.png',
+                      width: 100.0,
+                      height: 100.0,
+                    ),
+                    Text('DPS')
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const RoleContainer(
+            child: Expanded(
+              child: Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam porta dui vitae diam volutpat, at volutpat leo sodales. Sed pulvinar libero eget elit interdum gravida. Suspendisse potenti. Quisque lacinia lacinia elit, non aliquet ipsum ullamcorper sed. Duis libero urna, egestas scelerisque diam ut, interdum vulputate erat. Maecenas sed dui nec justo aliquam scelerisque. Donec et tellus vitae mi mattis facilisis. Sed imperdiet leo non aliquet egestas. In orci risus, tincidunt quis porta sit amet, euismod at enim.',
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
