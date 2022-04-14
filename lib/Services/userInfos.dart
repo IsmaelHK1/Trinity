f18c707c467a073fbf1b26613acfa9d1e2efade8
Aomine <robin.vitte@ynov.com>
2022-04-14 16:52:21 +0200

    Updapte getUserData

--- lib/Services/userInfos.dart
+++ lib/Services/userInfos.dart
@@ -1,9 +1,9 @@
-/*import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart';
 import 'package:trinity/Common/models/UserData.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<UserData> getUser(String uid) async {
-  UserData userVal;
+  late UserData userVal;
   final FirebaseFirestore _fireuser = FirebaseFirestore.instance;
 
   try {
@@ -18,4 +18,3 @@ Future<UserData> getUser(String uid) async {
 
   return userVal;
 }
-*/
