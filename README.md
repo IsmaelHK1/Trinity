b4f046590effac2389aee561929beb6231eb37c1
Thomas D <72820355+Kierha@users.noreply.github.com>
2022-04-15 17:18:53 +0200

    modif userPreference

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-### Trinity
+# Trinity
+
+Ce projet est à l'issue du Projet Dev pour la seconde année à Ynov filière Informatique.
+
+## Présentation 
+
+Trinity est un projet sur plateforme mobile qui a pour objectif de proposer un service d'accompagnement ludique durant les soirées.
+Elle s'adresse donc principalement aux jeunes,
 
 ### Initialisation du projet Trinity
 
--- lib/Common/utils/user_preferences.dart
+++ lib/Common/utils/user_preferences.dart
@@ -3,11 +3,10 @@ import 'package:trinity/Common/models/UserData.dart';
 class UserPreferences {
   static UserData myUser = UserData(
     imagepath:
-        'https://lvdneng.rosselcdn.net/sites/default/files/dpistyles_v2/ena_16_9_extra_big/2021/06/08/node_1022105/51568238/public/2021/06/08/B9727254842Z.1_20210608105519_000%2BGB5I8Q8O1.2-0.jpg?itok=NNfMFl0v1623142569',
-    pseudo: 'Robin Root',
-    email: 'Root@gmail.com',
-    birthday: '25 ans',
-    about:
-        'Certified Personal Trainer and Nutritionist with years of experience in creating effective diets and training plans focused on achieving individual customers goals in a smooth way.',
+        'https://www.google.com/url?sa=i&url=https%3A%2F%2Ficon-icons.com%2Ffr%2Ficone%2Fpersonne%2F50075&psig=AOvVaw2TPhzKU9xWzDYEys83u7WX&ust=1650122115887000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCKiMnYiulvcCFQAAAAAdAAAAABAJ',
+    pseudo: 'kierhann',
+    email: 'kierhann@gmail.com',
+    birthday: '10-03-1997',
+    about: 'Joueur de jeux vidéos en école de développement',
   );
 }
